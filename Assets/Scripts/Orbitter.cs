21eaff404e627e0fecae7962531982926d4cbdef
Farhan <far20shid@gmail.com>
2025-05-30 13:04:07 +0600

    tstijghn

--- Assets/Scripts/Orbitter.cs
+++ Assets/Scripts/Orbitter.cs
@@ -7,6 +7,7 @@ public class Orbitter : MonoBehaviour
     Rigidbody body;
     [Range(-20,20)]
     [SerializeField]private float offset = 20;
+    [SerializeField] private Transform player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,21 +22,49 @@ public class Orbitter : MonoBehaviour
 
     private void Update()
     {
-        float desiredDistanceFromCenter = Planet.Instance.transform.localScale.y * 0.5f + offset;
-        Vector3 up = (transform.position - Planet.Instance.GetPlanetPos()).normalized;
-        Vector3 targetPosition = Planet.Instance.GetPlanetPos() + up * desiredDistanceFromCenter;
-        Vector3 correctedPos = Vector3.Lerp(body.position, targetPosition, 0.1f);
-        Vector3 forward = Vector3.Cross(transform.right, up).normalized;
-        Vector3 move = forward * speed * Time.deltaTime;
-        body.MovePosition(correctedPos + move);
+        //PLANET ER Y AXIS DIYE ROTATING
+      //  Tangential();
 
-    //    body.AddForce(  (( correctedPos + forward) - transform.position).normalized * speed * Time.deltaTime);
-        
+        OrbiterLogic();
+
+        //    body.AddForce(  (( correctedPos + forward) - transform.position).normalized * speed * Time.deltaTime);
+
         //  body.MovePosition(body.position + (forward*1).normalized *speed*Time.deltaTime);
 
 
 
 
 
+    }
+
+    private void Tangential()
+    {
+        float desiredDistanceFromCenter = Planet.Instance.transform.localScale.y * 0.5f + offset;
+        Vector3 up = (transform.position - Planet.Instance.GetPlanetPos()).normalized;
+        Vector3 toPlayer = (player.position - transform.position).normalized;
+        Vector3 tangentToPlayer = Vector3.ProjectOnPlane(toPlayer, up);
+        Vector3 orbitDir = Vector3.Cross(up, tangentToPlayer).normalized;
+        Debug.DrawLine(transform.position, transform.position + tangentToPlayer.normalized * 5, Color.yellow);
+        Debug.DrawLine(transform.position, transform.position + orbitDir.normalized * 5, Color.blue);
+
+        body.MovePosition(transform.position + orbitDir * speed * Time.fixedDeltaTime);
+
+
+        float waterLevelRadius = Planet.Instance.GetPlanetScale() * .5f + offset;
+        // transform.position = Vector3.zero + (transform.position - Vector3.zero).normalized * waterLevelRadius;
+    }
+
+    private void OrbiterLogic()
+    {
+        //PLANET ER X AXIS DIYE ROTATING
+        float desiredDistanceFromCenter = Planet.Instance.transform.localScale.y * 0.5f + offset;
+        Vector3 up = (transform.position - Planet.Instance.GetPlanetPos()).normalized;
+        Vector3 targetPosition = Planet.Instance.GetPlanetPos() + up * desiredDistanceFromCenter;
+
+
+        Vector3 correctedPos = Vector3.Lerp(body.position, targetPosition, 0.1f);
+        Vector3 forward = Vector3.Cross(transform.right, up).normalized;
+        Vector3 move = forward * speed * Time.deltaTime;
+        body.MovePosition(correctedPos + move);
     }
 }
